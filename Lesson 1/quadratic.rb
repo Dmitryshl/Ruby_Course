6670f40edc2608fe862a35f7717305cd310d8a4b
Dmitryshl <Dmitrysh47@gmail.com>
2021-01-28 21:08:44 +0300

    First

---
+++ Lesson 1/quadratic.rb
@@ -0,0 +1,18 @@
+puts "Введи значение 'a'"
+a = gets.chomp.to_f
+puts "Введи значение 'b'"
+b = gets.chomp.to_f
+puts "Веди значение 'c'"
+c = gets.chomp.to_f
+d = b** 2 - 4 * a * c
+if d < 0
+  puts "Решения нет"
+elsif d== 0
+  x = - b/2*a
+  puts "Ответ: x = #{x}, дискрименант: #{d}"
+else
+  c = Math.sqrt(d)
+  x1 = (-b + c)/(2*a)
+  x2 = (-b - c)/(2*a)
+  puts "Ответ: x1 = #{x1}, x2 = #{x2}, дискрименант: #{d} "
+end
---
+++ Lesson 1/weight.rb
@@ -0,0 +1,12 @@
+
+puts 'Введите ваше имя'
+name = gets.chomp
+puts "Привет #{name}, напиши свой рост в см."
+height = gets.chomp.to_i
+puts "Напиши свой вес в кг."
+weight = gets.chomp.to_i
+if (height - 110)*1.15 < weight
+  puts "#{name} Твой вес не оптимальный"
+else
+  puts "#{name} Твой вес оптимальный"
+end
